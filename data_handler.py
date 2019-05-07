749b5d8a2943c65484783534e987edf4a16506f4
Bence Kovács <kovacs.ben.p@gmail.com>
2019-05-07 14:12:33 +0200

    skeleton code added, '/list' works, route for /question/<question_id> added

---
+++ data_handler.py
@@ -0,0 +1,10 @@
+import csv
+
+field = ['id','submission_time','view_number','vote_number','title','message','image']
+
+def get_all_questions():
+
+    with open("sample_data/question.csv", "r") as file:
+        all_questions = list(csv.DictReader(file))
+
+    return all_questions
---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, request
+import data_handler
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/list')
+def route_list():
+
+    all_questions = data_handler.get_all_questions()
+
+
+    return render_template('list.html', all_questions=all_questions)
+
+@app.route('/question/<question_id>')
+def route_question(question_id):
+
+    return render_template('question_details.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host = '0.0.0.0',
+        port = 8000,
+        debug = True
+    )
