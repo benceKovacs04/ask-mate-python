8a99ad5a8dc8bf123db6f8d9b6ad7512a096809b
Attila Suranyi <atis1203@gmail.com>
2019-05-07 16:05:39 +0200

    Question details are shown on the /question/id page

--- data_handler.py
+++ data_handler.py
@@ -2,9 +2,15 @@ import csv
 
 field = ['id','submission_time','view_number','vote_number','title','message','image']
 
-def get_all_questions():
 
+def get_all_questions():
     with open("sample_data/question.csv", "r") as file:
         all_questions = list(csv.DictReader(file))
 
     return all_questions
+
+
+def get_question(all_questions, id):
+    for question in all_questions:
+        if question['id'] == id:
+            return question
--- server.py
+++ server.py
@@ -3,24 +3,26 @@ import data_handler
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/list')
 def route_list():
-
     all_questions = data_handler.get_all_questions()
-
 
     return render_template('list.html', all_questions=all_questions)
 
+
 @app.route('/question/<question_id>')
 def route_question(question_id):
+    all_questions = data_handler.get_all_questions()
+    question = data_handler.get_question(all_questions, question_id)
 
-    return render_template('question_details.html')
+    return render_template('question_details.html', question=question)
 
 
 if __name__ == '__main__':
     app.run(
-        host = '0.0.0.0',
-        port = 8000,
-        debug = True
+        host='0.0.0.0',
+        port=8000,
+        debug=True
     )
