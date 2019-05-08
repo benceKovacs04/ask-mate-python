2f2dc45d227643d4e09643d13411430f6e55c482
kdani782 <kdani782@gmail.com>
2019-05-08 10:16:25 +0200

    displays all answers on /question page

--- connection.py
+++ connection.py
@@ -2,11 +2,11 @@ import csv
 
 HEADER = ['id','submission_time','view_number','vote_number','title','message','image']
 
-def get_all_questions():
-    with open('sample_data/question.csv', "r") as file:
-        all_questions = list(csv.DictReader(file))
+def get_all_data_from_file(file_name):
+    with open(file_name, "r") as file:
+        all_data = list(csv.DictReader(file))
 
-    return all_questions
+    return all_data
 
 def write_to_csv(file_name, data):
     #writeable_format = []
--- server.py
+++ server.py
@@ -8,16 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    all_questions = connection.get_all_questions()
+    all_questions = connection.get_all_data_from_file('sample_data/question.csv')
+
 
     return render_template('list.html', all_questions=all_questions)
-
 
 @app.route('/question/<question_id>')
 def route_question(question_id):
     question = data_handler.show_question(question_id)
+    all_answers = connection.get_all_data_from_file('sample_data/answer.csv')
 
-    return render_template('question_details.html', question=question)
+    return render_template('question_details.html', question=question, all_answers=all_answers)
 
 
 if __name__ == '__main__':
